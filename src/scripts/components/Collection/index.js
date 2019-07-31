adc3f6b33be2abd312480e6d53739975667279c6
Ian Springer <ian.springer@bvaccel.com>
2019-07-31 12:56:08 -0700

    filter logic

--- src/scripts/components/Collection/index.js
+++ src/scripts/components/Collection/index.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource'
-Vue.use(VueResource);
 
 const collection = new Vue({
 	el: '#collection',
@@ -8,9 +6,32 @@ const collection = new Vue({
 	data: {
 		title: '',
 		products: [],
-		filters: []
+		productsToShow: [],
+		colorFilters: [],
+		shellFilters: [],
+		sizeFilters: [],
+		filterSelected: false
 	},
 	methods: {
+		filterClick: function(filter) {
+			filter.isActive = true;
+			if(!this.filterSelected) {
+				this.productsToShow = this.products.filter(product => product[filter.type] == filter.value)
+			} else {
+				console.log(filter.type, filter.value)
+				this.productsToShow = this.productsToShow.filter(product => product[filter.type] == filter.value)
+			}
+			this.filterSelected = true;
+		},
+
+		reset: function() {
+			this.filterSelected = false;
+			this.productsToShow = this.products
+			this.shellFilters.map(item => item.active = false);
+			this.sizeFilters.map(item => item.active = false)
+			this.colorFilters.map(item => item.active = false)
+		},
+
 		getCollectionData: function(url) {
 
 			fetch(url)
@@ -18,20 +39,35 @@ const collection = new Vue({
 			    return response.json();
 			  })
 			  .then(function(response) {
-			  	const filterSet = new Set()
+			  	let allSizes = new Set();
+			  	let allShells = new Set();
+			  	let allColors = new Set();
 			    collection.title = response.title
 			    response.products.map(function(item) {
-			    	console.log('item', item)
 			    	collection.products.push(item)
-			    	filterSet.add(item.shell)
-			    	filterSet.add(item.color)
-			    	collection.filters = Array.from(filterSet)
+			    	collection.productsToShow.push(item)
+			    	item.shell.map(item => allShells.add(item))
+			    	item.color.map(item => allColors.add(item))
+			    	item.size.map(item => allSizes.add(item))
+			    	return item
 			    })
+			    Array.from(allColors).map(function(item) {
+			    	const obj = {type: 'color', value: item, isActive: false}
+			    	return collection.colorFilters.push(obj)
+			    });
+			    Array.from(allShells).map(function(item) {
+			    	const obj = {type: 'shell', value: item, isActive: false}
+			    	return collection.shellFilters.push(obj)
+			    });
+			    Array.from(allSizes).map(function(item) {
+			    	const obj = {type: 'size', value: item, isActive: false}
+			    	return collection.sizeFilters.push(obj)
+			    });
 			  });
 		}
 	},
 	created() {
-		this.getCollectionData(`/collections/drum-kits?view=json`); 
+		this.getCollectionData(`/collections/${window.collectionHandle}?view=json`); 
 	}
 })
 
