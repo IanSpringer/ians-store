2e665a7b9e3743c6a4ea417bb355a6a217e81032
Ian Springer <ian.springer@bvaccel.com>
2019-07-31 15:54:30 -0700

    loading state, pushState

--- src/scripts/components/Collection/index.js
+++ src/scripts/components/Collection/index.js
@@ -10,13 +10,16 @@ const collection = new Vue({
 		colorFilters: [],
 		shellFilters: [],
 		sizeFilters: [],
-		filterSelected: false
+		filterSelected: false,
+		collectionList: [],
+		loading: true,
 	},
 	methods: {
 		filterClick: function(filter) {
 			filter.isActive = true;
 			if(!this.filterSelected) {
-				this.productsToShow = this.products.filter(product => product[filter.type] == filter.value)
+				console.log(filter)
+				this.productsToShow = this.products.filter(product => product[filter.type].indexOf(filter.value) != -1)
 			} else {
 				console.log(filter.type, filter.value)
 				this.productsToShow = this.productsToShow.filter(product => product[filter.type] == filter.value)
@@ -26,48 +29,76 @@ const collection = new Vue({
 
 		reset: function() {
 			this.filterSelected = false;
-			this.productsToShow = this.products
-			this.shellFilters.map(item => item.active = false);
-			this.sizeFilters.map(item => item.active = false)
-			this.colorFilters.map(item => item.active = false)
+			this.productsToShow = this.products;
+			this.shellFilters.map(item => item.isActive = false);
+			this.sizeFilters.map(item => item.isActive = false);
+			this.colorFilters.map(item => item.isActive = false);
 		},
 
-		getCollectionData: function(url) {
+		buildProductGrid: function(response) {
+	  	let allSizes = new Set();
+	  	let allShells = new Set();
+	  	let allColors = new Set();
+	    collection.title = response.title
+	    response.products.map(function(item) {
+	    	collection.products.push(item)
+	    	collection.productsToShow.push(item)
+	    	item.shell.map(item => allShells.add(item))
+	    	item.color.map(item => allColors.add(item))
+	    	item.size.map(item => allSizes.add(item))
+	    	return item
+	    })
+	    Array.from(allColors).map(function(item) {
+	    	const obj = {type: 'color', value: item, isActive: false}
+	    	return collection.colorFilters.push(obj)
+	    });
+	    Array.from(allShells).map(function(item) {
+	    	const obj = {type: 'shell', value: item, isActive: false}
+	    	return collection.shellFilters.push(obj)
+	    });
+	    Array.from(allSizes).map(function(item) {
+	    	const obj = {type: 'size', value: item, isActive: false}
+	    	return collection.sizeFilters.push(obj)
+	    });
+		},
 
-			fetch(url)
+		buildCollectionData: function(response) {
+			response.collections.map(item => {
+				const obj = {title: item.split(':')[0], handle: item.split(':')[1]}
+				collection.collectionList.push(obj);
+			})
+		},
+
+		clearState: function(handle) {
+			this.products = [];
+			this.productsToShow = [];
+			this.colorFilters = [];
+			this.shellFilters = [];
+			this.sizeFilters = [];
+			this.filterSelected = false;
+			this.collectionList = [];
+			this.loading = true;
+			history.pushState(null, null, `https://localhost:3000${handle}`)
+			return this.getCollectionData(handle)
+		},
+
+		getCollectionData: function(handle) {
+			const collectionURL = `${handle}?view=json`
+			fetch(collectionURL)
 			  .then(function(response) {
 			    return response.json();
 			  })
 			  .then(function(response) {
-			  	let allSizes = new Set();
-			  	let allShells = new Set();
-			  	let allColors = new Set();
-			    collection.title = response.title
-			    response.products.map(function(item) {
-			    	collection.products.push(item)
-			    	collection.productsToShow.push(item)
-			    	item.shell.map(item => allShells.add(item))
-			    	item.color.map(item => allColors.add(item))
-			    	item.size.map(item => allSizes.add(item))
-			    	return item
-			    })
-			    Array.from(allColors).map(function(item) {
-			    	const obj = {type: 'color', value: item, isActive: false}
-			    	return collection.colorFilters.push(obj)
-			    });
-			    Array.from(allShells).map(function(item) {
-			    	const obj = {type: 'shell', value: item, isActive: false}
-			    	return collection.shellFilters.push(obj)
-			    });
-			    Array.from(allSizes).map(function(item) {
-			    	const obj = {type: 'size', value: item, isActive: false}
-			    	return collection.sizeFilters.push(obj)
-			    });
-			  });
+			  	collection.buildProductGrid(response)
+			  	collection.buildCollectionData(response)
+					setTimeout(function() {
+						collection.loading = false
+					}, 1500)
+			  })
 		}
 	},
 	created() {
-		this.getCollectionData(`/collections/${window.collectionHandle}?view=json`); 
+		this.getCollectionData(window.collectionHandle); 
 	}
 })
 
